c18088f01a9dacb730a3e4e4893a4793068043e8
Jay Schwane <jrschwane@uwalumni.com>
2019-03-24 11:22:46 -0500

    Adds common express router test fixture

---
+++ spec/fixtures/req-res-next.js
@@ -0,0 +1,23 @@
+/**
+ * mocked request route parameters
+ */
+
+const req = {
+  body: {},
+  params: {},
+  token: {},
+  log: {
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn()
+  }
+}
+
+const res = {
+  status: jest.fn(function () { return this }),
+  json: jest.fn()
+}
+
+const next = jest.fn()
+
+exports = module.exports = { req, res, next }
