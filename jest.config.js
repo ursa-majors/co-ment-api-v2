8e01bf60f45b440b980e53712cd9f22bac7e9a68
Jay Schwane <jrschwane@uwalumni.com>
2019-03-30 09:14:48 -0500

    Moves Jest config into own file

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  },
+  roots: [
+    '<rootDir>/src/',
+    '<rootDir>/spec/'
+  ],
+  testEnvironment: 'node',
+  verbose: true,
+  watchPathIgnorePatterns: [
+    '<rootDir>/node_modules/'
+  ]
+}
