41692d95010b8e181e83058fe88a8d93c8b3c5dd
Jay Schwane <jrschwane@uwalumni.com>
2019-04-19 08:55:48 -0500

    Begin adding User model validations & unit tests

--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -37,16 +37,166 @@ const mockUser = {
 }
 
 describe(`User model`, () => {
-  let user
+  let testUser
 
   beforeEach(() => {
-    user = _cloneDeep(mockUser)
+    testUser = _cloneDeep(mockUser)
   })
 
-  it(`should validate 'username' type`, async () => {
-    user.username = { wrong: 'type' }
-    const u = new User(user)
-    await expect(u.validate()).rejects
-      .toThrow(/User validation failed: username/)
+  describe('username validations', () => {
+    const errMsg = /User validation failed: username/
+
+    it(`should be OK with valid usernames`, async () => {
+      testUser.username = 'this_IS-valid666'
+      const u = new User(testUser)
+      await expect(u.validate()).resolves
+    })
+
+    it(`should error if missing`, async () => {
+      delete testUser.username
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on invalid type`, async () => {
+      testUser.username = { wrong: 'type' }
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on string with no length`, async () => {
+      testUser.username = ''
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on string length > 32 chars`, async () => {
+      testUser.username = '0123456789abcdef0123456789abcdef!'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on string with leading numbers`, async () => {
+      testUser.username = '6_no_leading_numbers'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on string with leading underscore`, async () => {
+      testUser.username = '_no_leading_underscore'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on string with inner whitespace`, async () => {
+      testUser.username = 'no spaces'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+  })
+
+  describe('email validations', () => {
+    const errMsg = /User validation failed: email/
+
+    it(`should be OK with valid email addresses`, async () => {
+      testUser.email = 'this_IS@valid-email.com'
+      const u = new User(testUser)
+      await expect(u.validate()).resolves
+    })
+
+    it(`should lowercase valid email addresses`, async () => {
+      testUser.email = 'THIS_IS@UPPER-CASE.com'
+      const u = new User(testUser)
+      await expect(u.validate()).resolves
+      expect(u.email).toEqual(testUser.email.toLowerCase())
+    })
+
+    it(`should error if missing`, async () => {
+      delete testUser.email
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on invalid type`, async () => {
+      testUser.email = { wrong: 'type' }
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on email with no length`, async () => {
+      testUser.email = ''
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on string length > 64 chars`, async () => {
+      testUser.email = 'this_is_a_long_email_address@some-huge-domain-name-really-wow.com'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on invalid string pattern`, async () => {
+      testUser.email = 'not-an-email.com'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+  })
+
+  describe('name validations', () => {
+    const errMsg = /User validation failed: name/
+
+    it(`should be OK with valid name`, async () => {
+      testUser.name = 'Test User Name'
+      const u = new User(testUser)
+      await expect(u.validate()).resolves
+    })
+
+    it(`should permit non-ASCII characters`, async () => {
+      testUser.name = 'Andrés Düsseldorf Москва Köln'
+      const u = new User(testUser)
+      await expect(u.validate()).resolves
+    })
+
+    it(`should error on invalid type`, async () => {
+      testUser.name = { wrong: 'type' }
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on email with no length`, async () => {
+      testUser.name = ''
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on string length > 64 chars`, async () => {
+      testUser.name = 'this name is otherwise valid but waaaaaaaaaaaaaaaaaaaaay too long'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on strings with < brackets >`, async () => {
+      testUser.name = '<cant have brackets>'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on strings with { curly braces }`, async () => {
+      testUser.name = '{cant have braces}'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on strings with ( parens )`, async () => {
+      testUser.name = 'functionCall()'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
+
+    it(`should error on strings with $ signs`, async () => {
+      testUser.name = '$set: { fail: true }'
+      const u = new User(testUser)
+      await expect(u.validate()).rejects.toThrow(errMsg)
+    })
   })
 })
--- src/models/user/index.js
+++ src/models/user/index.js
@@ -5,37 +5,121 @@ const crypto = require('crypto')
 const jwt = require('jsonwebtoken')
 const jwtSecret = process.env.JWT_SECRET
 
+// @TODO -- LOTS OF VALIDATIONS!!!
+
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  email: { type: String, unique: true, required: true },
-  name: { type: String, trim: true },
-  ghUserName: { type: String, trim: true },
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+    minlength: 2,
+    maxlength: 32,
+    match: [
+      /^[a-zA-Z]+([.-]?\w+)*$/,
+      'Valid formats: user-name, user_name, Username, userName123'
+    ]
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: true,
+    maxlength: 64,
+    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/
+  },
+  name: {
+    type: String,
+    trim: true,
+    minlength: 2,
+    maxlength: 64,
+    match: /[^{}[\]\r\n()<>?$]+$/
+  },
+  ghUserName: {
+    type: String,
+    trim: true
+  },
   ghProfile: Object, // do we use this ?????
-  avatarUrl: { type: String, trim: true },
-  location: { type: String, trim: true },
-  about: { type: String, trim: true },
-  gender: { type: String, trim: true },
-  github: { type: String, trim: true },
-  twitter: { type: String, trim: true },
-  facebook: { type: String, trim: true },
-  link: { type: String, trim: true },
-  linkedin: { type: String, trim: true },
-  codepen: { type: String, trim: true },
-  signupKey: { key: String, ts: String, exp: String },
-  passwordResetKey: { key: String, ts: String, exp: String },
-  validated: { type: Boolean, default: false },
+  avatarUrl: {
+    type: String,
+    trim: true
+  },
+  location: {
+    type: String,
+    trim: true
+  },
+  about: {
+    type: String,
+    trim: true
+  },
+  gender: {
+    type: String,
+    trim: true
+  },
+  github: {
+    type: String,
+    trim: true
+  },
+  twitter: {
+    type: String,
+    trim: true
+  },
+  facebook: {
+    type: String,
+    trim: true
+  },
+  link: {
+    type: String,
+    trim: true
+  },
+  linkedin: {
+    type: String,
+    trim: true
+  },
+  codepen: {
+    type: String,
+    trim: true
+  },
+  signupKey: {
+    key: String,
+    ts: String,
+    exp: String
+  },
+  passwordResetKey: {
+    key: String,
+    ts: String,
+    exp: String
+  },
+  validated: {
+    type: Boolean,
+    default: false
+  },
   languages: [String],
   certs: [String],
   skills: [String],
-  time_zone: { type: String, trim: true },
+  time_zone: {
+    type: String,
+    trim: true
+  },
   likedPosts: [String],
   contactMeta: {
-    unSubbed: { type: Boolean, default: false },
-    alreadyContacted: { type: Boolean, default: false }
+    unSubbed: {
+      type: Boolean,
+      default: false
+    },
+    alreadyContacted: {
+      type: Boolean,
+      default: false
+    }
   },
   engagementMeta: {
-    addPostReminder: { type: Date },
-    addProfileReminder: { type: Date }
+    addPostReminder: {
+      type: Date
+    },
+    addProfileReminder: {
+      type: Date
+    }
   },
   hash: { type: String, select: false },
   salt: { type: String, select: false }
